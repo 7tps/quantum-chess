bdb7935da7e3e736e8fc3770d77a9ef2bfaf5f9d
Mikhil Kiran <109924342+7tps@users.noreply.github.com>
2025-10-16 03:18:31 -0700

    added assets + basic code

---
+++ Assets/Scripts/ChessEngine.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ChessEngine : MonoBehaviour
+{
+
+    public Sprite[] whiteChessSprites;
+    public Sprite[] blackChessSprites;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ChessPiece.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public class ChessPiece : MonoBehaviour
+{
+
+    public enum pieceType
+    {
+        Pawn,
+        Knight,
+        Bishop,
+        Rook,
+        Queen,
+        King
+    }
+
+    public ChessEngine engine;
+    
+    public SpriteRenderer sr;
+    public Sprite sprite;
+
+    public bool isWhite = true;
+    public pieceType type;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (isWhite)
+        {
+            sprite = engine.whiteChessSprites[(int) type];
+        }
+        else
+        {
+            sprite = engine.blackChessSprites[(int) type];
+        }
+        
+        sr.sprite = sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
